c4116bb884c0321f451e71e4586e267df43c652a
Andrei <andryushik@gmail.com>
2023-03-06 13:47:35 +0100

    styles and refactoring

--- app.js
+++ app.js
@@ -4,31 +4,34 @@ window.addEventListener('load', () => {
   const location = document.querySelector('.location');
   const iconImgSet = document.querySelector('img');
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
       const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=dbe8033764867f4247db7100905000c1&units=metric`;
 
-      try {
-        fetch(api)
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            const descriptionString =
-              data.weather[0].description.charAt(0).toUpperCase() +
-              data.weather[0].description.slice(1);
-            iconImgSet.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-            temperature.textContent = Math.round(data.main.temp);
-            description.innerHTML = `${descriptionString},<br> wind ${data.wind.speed} m/s`;
-            location.textContent = data.name;
-          });
-      } catch (error) {
-        throw new Error('Cannot get the weather data');
-      }
-    });
-  } else {
-    location.textContent = 'Cannot find your location';
-  }
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const descriptionString =
+            data.weather[0].description.charAt(0).toUpperCase() +
+            data.weather[0].description.slice(1);
+          iconImgSet.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+          temperature.textContent = Math.round(data.main.temp);
+          description.innerHTML = `${descriptionString},<br> wind ${data.wind.speed} m/s`;
+          location.textContent = data.name;
+        })
+        .catch((error) => {
+          alert('Cannot get the weather data! Check your internet connection.');
+          throw new Error('Cannot get the weather data');
+        });
+    },
+    (error) => {
+      console.log('cant get location', error.message);
+      alert('Cannot get your location. Check browser settings.');
+      location.textContent = 'Cannot find your location';
+    },
+  );
 });
